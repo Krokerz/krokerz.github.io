21900d7585655ea5663bf940c28c116778d13112
KevinOctavius <s4013108@student.rmit.edu.au>
2025-03-04 20:28:23 +1100

    Made properties compatible with react

--- pages/projects/article-builder/article-builder.tsx
+++ pages/projects/article-builder/article-builder.tsx
@@ -5,68 +5,68 @@ import './article-builder.css'
 export default function ArticleBuilder() {
     return (
         <>
-            <div class="article-builder" id="article-builder">
-                <div class="main-article-info" id="main-article-info">
-                    <div class="article-title-container">
-                        <h1 id="article-title" placeholder="Insert Title Here" contenteditable="true"></h1>
-                        <div class="modify-text" id="modify-text-title">
-                            <button class="article-italic-toggle" data-checked="false">
+            <div className="article-builder" id="article-builder">
+                <div className="main-article-info" id="main-article-info">
+                    <div className="article-title-container">
+                        <h1 id="article-title" data-placeholder="Insert Title Here" contentEditable="true"></h1>
+                        <div className="modify-text" id="modify-text-title">
+                            <button className="article-italic-toggle" data-checked="false">
                                 <img src="/img/italic.svg" alt="Italic" />
                             </button>
-                            <button class="article-strikethrough-toggle" data-checked="false">
+                            <button className="article-strikethrough-toggle" data-checked="false">
                                 <img src="/img/strikethrough.svg" alt="Strikethrough" />
                             </button>
-                            <button class="article-underline-toggle" data-checked="false">
+                            <button className="article-underline-toggle" data-checked="false">
                                 <img src="/img/underline.svg" alt="Underline" />
                             </button>
                         </div>
                     </div>
-                    <div class="article-desc-container">
-                        <h2 id="article-desc" placeholder="Insert Description Here" contenteditable="true"></h2>
-                        <div class="modify-text" id="modify-text-desc">
-                            <button class="article-bold-toggle" data-checked="false">
+                    <div className="article-desc-container">
+                        <h2 id="article-desc" data-placeholder="Insert Description Here" contentEditable="true"></h2>
+                        <div className="modify-text" id="modify-text-desc">
+                            <button className="article-bold-toggle" data-checked="false">
                                 <img src="/img/bold.svg" alt="Bold" />
                             </button>
-                            <button class="article-strikethrough-toggle" data-checked="false">
+                            <button className="article-strikethrough-toggle" data-checked="false">
                                 <img src="/img/strikethrough.svg" alt="Strikethrough" />
                             </button>
-                            <button class="article-underline-toggle" data-checked="false">
+                            <button className="article-underline-toggle" data-checked="false">
                                 <img src="/img/underline.svg" alt="Underline" />
                             </button>
                         </div>
                     </div>
                 </div>
-                <div class="side-article-info">
+                <div className="side-article-info">
                     <time id="time">dd/Month/yyyy</time>
-                    <div class="article-tags">
-                        <div class="tags" id="tags">
-                            <div placeholder="?" contenteditable="true"></div>
+                    <div className="article-tags">
+                        <div className="tags" id="tags">
+                            <div data-placeholder="?" contentEditable="true"></div>
                         </div>
-                        <div class="tag-buttons" id="tag-buttons">
+                        <div className="tag-buttons" id="tag-buttons">
                             <button>Add Tag</button>
                             <button>Delete Tag</button>
                         </div>
                     </div>
                 </div>
                 <hr />
-                <div class="article-paragraphs-container">
-                    <p id="article-paragraphs" placeholder="Insert Paragraph(s) Here" contenteditable="true"></p>
-                    <div class="modify-text" id="modify-text-para">
-                        <button class="article-bold-toggle" data-checked="false">
+                <div className="article-paragraphs-container">
+                    <p id="article-paragraphs" data-placeholder="Insert Paragraph(s) Here" contentEditable="true"></p>
+                    <div className="modify-text" id="modify-text-para">
+                        <button className="article-bold-toggle" data-checked="false">
                             <img src="/img/bold.svg" alt="Bold" />
                         </button>
-                        <button class="article-italic-toggle" data-checked="false">
+                        <button className="article-italic-toggle" data-checked="false">
                             <img src="/img/italic.svg" alt="Italic" />
                         </button>
-                        <button class="article-strikethrough-toggle" data-checked="false">
+                        <button className="article-strikethrough-toggle" data-checked="false">
                             <img src="/img/strikethrough.svg" alt="Strikethrough" />
                         </button>
-                        <button class="article-underline-toggle" data-checked="false">
+                        <button className="article-underline-toggle" data-checked="false">
                             <img src="/img/underline.svg" alt="Underline" />
                         </button>
-                        <div class="article-font-size-container">
-                            <div class="article-font-size-display" id="article-font-size-display" contenteditable="true"></div>
-                            <div class="article-font-size-buttons" id="article-font-size-buttons">
+                        <div className="article-font-size-container">
+                            <div className="article-font-size-display" id="article-font-size-display" contentEditable="true"></div>
+                            <div className="article-font-size-buttons" id="article-font-size-buttons">
                                 <button>
                                     <img src="/img/up.svg" alt="Increment" />
                                 </button>
@@ -78,7 +78,7 @@ export default function ArticleBuilder() {
                     </div>
                 </div>
             </div>
-            <button class="save-article" id="save-article">Save</button>
+            <button className="save-article" id="save-article">Save</button>
         </>
     );
 }
